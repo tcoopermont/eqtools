4689da19b852e51e51ebd64d6aed73b5ef91ee4e
Tom Cooper <tomcooper@tec8.net>
2017-07-15 18:35:48 -0700

    changed equal to is_a for map test

--- tests/testthat/test_map.R
+++ tests/testthat/test_map.R
@@ -7,7 +7,8 @@ test_that("eq_map returns a object with class leaflet",{
    cdf <- eq_clean_data(adf)
    #getting a warning about bad lat,long
    amap <- eq_map(cdf,annot_col = "DATE")
-   expect_equal(class(amap),c("leaflet","htmlwidget"))
+   expect_is(amap,"leaflet")
+   expect_is(amap,"htmlwidget")
 })
 
 test_that("eq_create_label returns correct data",{
