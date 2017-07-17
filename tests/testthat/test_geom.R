bf4f936b15390a4cd071cf472b9285bf0fcd717a
Tom Cooper <tomcooper@Toms-iMac.local>
2017-07-16 20:08:40 -0700

    fixed test geom, working on vig

--- tests/testthat/test_geom.R
+++ tests/testthat/test_geom.R
@@ -4,9 +4,13 @@ library(readr)
 library(leaflet)
 
 test_that("geom_timeline returns a object with class ggplot",{
+   minYear <- 2000		  
+   maxYear <- 2010		  
    adf <- read_delim(  system.file("extdata", "signif-july-2017.txt.gz", package = "eqtools"),
 		       delim = "\t")
-   cdf <- eq_clean_data(adf)
+   cdf <- eq_clean_data(adf) %>%
+          filter( YEAR >= minYear,YEAR <= maxYear, COUNTRY %in% c("USA","MEXICO")) %>%
+	  mutate(EQ_PRIMARY = as.numeric(EQ_PRIMARY))
    aplot <- ggplot(cdf,aes(x = YEAR, y = 1,
          xmin = minYear,xmax = maxYear,
          )) +
@@ -18,9 +22,13 @@ test_that("geom_timeline returns a object with class ggplot",{
 })
 
 test_that("geom_timeline_label returns a object with class ggplot",{
+   minYear <- 2000		  
+   maxYear <- 2010		  
    adf <- read_delim(  system.file("extdata", "signif-july-2017.txt.gz", package = "eqtools"),
 		       delim = "\t")
-   cdf <- eq_clean_data(adf)
+   cdf <- eq_clean_data(adf) %>%
+          filter( YEAR >= minYear,YEAR <= maxYear, COUNTRY %in% c("USA","MEXICO")) %>%
+	  mutate(EQ_PRIMARY = as.numeric(EQ_PRIMARY))
    aplot <- ggplot(cdf,aes(x = YEAR, y = 1,
          xmin = minYear,xmax = maxYear,
          )) +
